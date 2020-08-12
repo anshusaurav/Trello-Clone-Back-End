be03ec5688aa540e36c2ef57b9e2d39786e20a87
anshusaurav <anshu.saurav@gmail.com>
2020-08-12 14:18:50 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+This project was bootstrapped with [Express Generator](https://https://github.com/expressjs/generator).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000)
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `npm start`
+
+Launches the .<br />
---
+++ app.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var logger = require('morgan');
+var cors = require('cors');
+var errorhandler = require('errorhandler');
+var mongoose = require('mongoose');
+
+var isProduction = process.env.NODE_ENV === 'development'
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    session({
+        secret: 'trello',
+        cookie: { maxAge: 60000 },
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
+if (!isProduction) {
+    app.use(errorhandler())
+}
+
+if (isProduction) {
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+} else {
+    mongoose.connect('mongodb://localhost/trellodb', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.set('debug', true)
+}
+
+
+
+
+app.use(require('./routes'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+    app.use(function (err, req, res, next) {
+        console.log(err.stack)
+
+        res.status(err.status || 500)
+
+        res.json({
+            errors: {
+                message: err.message,
+                error: err
+            }
+        })
+    })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
+        errors: {
+            message: err.message,
+            error: {}
+        }
+    })
+})
+
+// finally, let's start our server...
+var server = app.listen(process.env.PORT || 4000, function () {
+    console.log('Listening on port ' + server.address().port)
+})
