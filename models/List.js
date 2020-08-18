26a179eaa5c892eb489f6902300f6fb9ca2b4049
anshusaurav <anshu.saurav@gmail.com>
2020-08-18 12:35:19 +0530

    Schema method for list to manage swaps

--- models/List.js
+++ models/List.js
@@ -38,7 +38,58 @@ ListSchema.methods.slugify = function () {
         "-" +
         ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
 };
+/**
+ * Move a card inside list from moveFrom to moveTo position.
+ * Consist of two steps
+ * Removing Card at moveFrom position
+ * Inserting it at movetoPosition and shift other elements
+ * accordingly.
+ * @param {*} moveFrom 
+ * @param {*} moveTo 
+ */
+ListSchema.methods.moveCard = function (moveFrom, moveTo) {
+    if (start > this.issues.length || end > this.issues.length || start < 0 || end < 0) {
+        return this;
+    }
+    let resArr = [...this.issues];
+    removedIssue = resArr.splice(moveFrom, 1);
+    resArr.splice(moveTo, 0, removedIssue);
+    this.issues = resArr;
+    return this;
+};
 
+
+/**
+ * add a card with "issueId" at position "position" in given list. If no position 
+ * is provided card will be added to the end
+ * @param {*} issueId 
+ * @param {*} position 
+ */
+ListSchema.methods.addCard = function (issueId, position = this.issues.length) {
+    if (position < 0 || position > issues.length) {
+        return this;
+    }
+    let resArr = [...this.issues];
+    resArr.splice(position, 0, issueId);
+    this.issues = resArr;
+    return this;
+};
+
+
+/**
+ * remove a card at position "position" in given list. 
+ * @param {*} issueId 
+ * @param {*} position 
+ */
+ListSchema.methods.removeCard = function (position) {
+    if (position < 0 || position > issues.length) {
+        return this;
+    }
+    let resArr = [...this.issues];
+    resArr.splice(position, 1);
+    this.issues = resArr;
+    return this;
+}
 ListSchema.methods.toListJSON = function () {
     return {
         id: this._id,
